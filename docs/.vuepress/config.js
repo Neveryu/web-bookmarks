45402a9c5c60b797d606c92007623fdf63e80070
yudong <never_yu@foxmail.com>
2019-11-03 21:46:46 +0800

    complete pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,9 +5,17 @@ module.exports = {
   description: '前端工程师知识技能储备库',
   base: '/web-bookmarks/',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
-  serviceWorker: false,
+  serviceWorker: true,
   themeConfig: {
     nav: [
       { 
